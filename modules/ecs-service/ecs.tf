1dd074a0249d77dd088bfd5106e4da4f87c2d8b5
nargok <nargok@gmail.com>
2025-05-14 16:49:45 +0900

    ECS taskdefinition

--- modules/ecs-service/ecs.tf
+++ modules/ecs-service/ecs.tf
@@ -4,3 +4,39 @@ resource "aws_cloudwatch_log_group" "this" {
 }
 
 data "aws_region" "this" {}
+
+resource "aws_ecs_task_definition" "this" {
+  family = var.name
+  container_definition = jsonencode([
+    {
+      name = var.name
+      image = var.image
+      environtment = var.env_vars
+      secrets = var.secrets
+      portMappings = [
+        {
+          containerPort = var.port
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group = aws_cloudwatch_log_group.this.name
+          awslogs-region = data.aws_region.this.name
+          awslogs-stream-prefix = var.name
+        }
+      }
+    }
+  ])
+  cpu = var.cpu
+  memory = var.memory
+  requires_compatibility = ["FARGATE"]
+  network_mode = "awsvpc"
+  execution_role_arn = var.execution_role_arn
+  task_role_arn = var.task_role_arn
+}
+
+data "aws_ecs_cluster" "this" {
+  cluster_name = var.cluster_name
+}
+
--- modules/ecs-service/variables.tf
+++ modules/ecs-service/variables.tf
@@ -1,1 +1,22 @@
 variable "log_retention_in_days" {}
+
+variable "name" {}
+
+variable "image" {}
+
+variable "env_vars" {}
+
+variable "secrets" {}
+
+variable "port" {}
+
+variable "cpu" {}
+
+variable "memory" {}
+
+variable "execution_role_arn" {}
+
+variable "task_role_arn" {}
+
+variable "cluster_name" {}
+
